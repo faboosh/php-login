3948a03d9f4b3e8587734d0c10999cddff4ffc51
Fabian Johansson <fabulo1998@gmail.com>
2020-01-25 20:01:09 +0100

    added frontend

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Application Login</h1>
+        <form action="secret.php" class="form">
+            <input placeholder="Enter your username" name="username" type="text">
+            <input placeholder="Enter your password" name="password" type="password">
+            <button type="submit">Login</button>
+        </form>
+        <a href="register.php">Sign up here!</a>
+    </div>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,29 @@
+<?php
+
+    if(isset($_POST['username'])) {
+        //om användarnamn
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Register</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Application Registration</h1>
+        <form action="register.php" class="form">
+            <input placeholder="Enter your username" name="username" type="text">
+            <input placeholder="Enter your password" name="password" type="password">
+            <button type="submit">Register</button>
+        </form>
+        <a href="index.php">Login here!</a>
+    </div>
+</body>
+</html>
